4f0492151baa89d2e7bfdd5dfa42ae15df846bf2
Sergii Koliada <sergii.koliada@gmail.com>
2011-01-30 13:17:18 +0200

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,25 @@
 SampleApp040111::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
   
-  get "pages/about"
+  # See text after Listing 5.29  http://railstutorial.org/chapters/filling-in-the-layout#fnref:5.9
+  # Note that we have kept the rule get "users/new", which was generated automatically by the Users
+  # controller generation in Listing 5.23. Currently, this rule is necessary to route /users/new correctly,
+  # but it doesn’t follow the proper REST conventions (Table 2.2), and we will eliminate it in Section 6.3.3.
+  get "users/new"
+  
+  match '/signup',  :to => 'users#new'
+
+  match '/contact', :to => 'pages#contact'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  
+  # These Pages controller routes inroduced in Listing 3.17 and excluded by Listing 5.18
+  # See 'Ruby on Rails Tutorial: Learn Rails by Example' by Michael Hartl
+  # get "pages/home"
+  # get "pages/contact"
+  # get "pages/about"
+  # get "pages/help"
+  
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -54,6 +70,7 @@ SampleApp040111::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+  root :to => 'pages#home'
 
   # See how all your routes lay out with "rake routes"
 
